5169e880fd162b88224f18980fa5023b2c610db5
Dinesh9657803055 <dinesh.kadam1509@gmail.com>
2025-03-17 17:10:52 +0530

    commit message

--- src/test/java/com/BHL/TC002_DashboardPageTest.java
+++ src/test/java/com/BHL/TC002_DashboardPageTest.java
@@ -112,6 +112,7 @@ public class TC002_DashboardPageTest extends BeforeAfterMethods {
 		Expected_Moudle_Approval_List.add("Deactivate Trips");
 		Expected_Moudle_Approval_List.add("Refueling Log");
 		Expected_Moudle_Approval_List.add("POD Documents");
+		System.out.println("test");
 		return Expected_Moudle_Approval_List;
 	}
 
